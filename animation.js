ffd0903c89ab7a4a2fca9ca4ce9fc156ed834ae8
trushka <trueshko@gmail.com>
2022-08-01 01:51:36 +0300

    add turning continuity

--- animation.js
+++ animation.js
@@ -24,17 +24,19 @@ requestAnimationFrame(function anim(){
 	 const y=y0+h0*(j+.5);
 	 for (let i=0; i<nx; i++) {
 		 const x=x0+w0*(i+.5),
+					 dash=$dash[j*nx+i],
+					 a0=dash._angle||0,
 					 dx=targets[0].x - x,
 					 dy=targets[0].y - y,
 					 r2=(dx*dx+dy*dy)/h0/w0/15;
 					 f=Math.atan(1/r2),//f=10*w0*h0/(dx*dx+dy*dy),
-					 dash=$dash[j*nx+i],
-					 angle=f*2,
-					 targ=angle+angleTo(angle, -Math.atan(dx/dy))*(1-f*r2),
+					 a1=f*2,
+					 a2=-Math.atan(dx/dy),
+					 da=angleTo(a1, a2)
+					 targ=dash._angle=a1+da*(1-f*r2)*(Math.cos(da)),
 					 //targ=-Math.atan(dx/dy)*(1-f*r2),
-					 targ0=dash._trg||0,
-					 a0=dash._angle||0;
-		 dash.style.transform=dash._angle=`rotate(${targ}rad)`
+					 targ0=dash._trg||0;
+		 dash.style.transform=`rotate(${targ}rad)`
 	 }
 	}
 	targets.length = 0;
