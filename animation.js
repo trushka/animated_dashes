46ab9d7877b5fec9d72478f779053e7374ab25a5
trushka <trueshko@gmail.com>
2022-08-06 05:16:25 +0300

    add dynamic

--- animation.js
+++ animation.js
@@ -4,39 +4,60 @@ const $win=$(window),
 			ny=$el.css('--h'),
 			n=nx*ny,
 			PI=Math.PI, PI2=PI/2, PIx2=PI*2, PI32=PI*1.5;
+
 for (let i=0; i<n; i++) {
 	$el.append('<div/>')
 };
-const $dash=$el.children(),
-			targets=[];
+
+const $dash=$el.children();
+let targets=[], t0=performance.now();
+
 $win.on('mousemove touchstart touchmove', function setTarg(e){
-	if (!e.touches) targets.push({x: e.clientX, y: e.clientY})
-	else for(let i=0; i<e.touches.length; i++) {setTarg(e.touches[i])};
+	if (!e.touches) targets=[{x: e.clientX, y: e.clientY}]
+	else targets=Array.from(e.touches[i]);
 })
+
 requestAnimationFrame(function anim(){
 	requestAnimationFrame(anim);
-	if (!targets[0]) return;
-	const box=$el[0].getBoundingClientRect(),
+
+	//if (!targets[0]) return;
+
+	const t=performance.now(),
+				dt=Math.min(50, t-t0)*.003,
+
+				box=$el[0].getBoundingClientRect(),
 				x0=box.left, y0=box.top,
 				w0=box.width/nx,
-				h0=box.height/ny;
+				h0=box.height/ny,
+				hw=h0*w0/4;
+
 	for (let j=0; j<ny; j++) {
 	 const y=y0+h0*(j+.5);
+
 	 for (let i=0; i<nx; i++) {
-		 const x=x0+w0*(i+.5),
-					 dash=$dash[j*nx+i],
-					 a0=dash._angle||0,
-					 dx=targets[0].x - x,
-					 dy=targets[0].y - y,
-					 r2=(dx*dx+dy*dy)/h0/w0/15;
-					 f=Math.atan(1/r2),//f=10*w0*h0/(dx*dx+dy*dy),
-					 a1=f*2,
+		const x=x0+w0*(i+.5),
+					dash=$dash[j*nx+i],
+					a0=dash._angle||0;
+	
+		let f=angleTo(a0, .16)*.05;
+
+		t0=t;
+
+	 	targets.forEach(target=>{
+			const dx=targets[0].x - x,
+						dy=targets[0].y - y,
+						r2=(dx*dx+dy*dy)/hw/100,
+
+					 a1=Math.atan(1/r2)*1.2,//f=10*w0*h0/(dx*dx+dy*dy),
+					 //a1=Math.min(f, PI2)+.16,
 					 a2=-Math.atan(dx/dy),
 					 da=angleTo(a1, a2)
-					 targ=dash._angle=a1+da*(1-f*r2)*(Math.cos(da)),
+					 //targ=dash._angle=a1+da*(Math.cos(da))*(1-f*r2),
 					 //targ=-Math.atan(dx/dy)*(1-f*r2),
 					 targ0=dash._trg||0;
-		 dash.style.transform=`rotate(${targ}rad)`
+			f+=angleTo(a0, a1);
+	 	})
+		dash.style.transform=`rotate(${dash._angle=a0+f*dt}rad)`
 	 }
 	}
 	targets.length = 0;
