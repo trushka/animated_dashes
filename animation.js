cf59e8db2b955d349803755968d517ffb2d90514
trushka <trueshko@gmail.com>
2022-08-11 05:51:24 +0300

    changed to canvas, improved effect parameters

--- animation.js
+++ animation.js
@@ -1,11 +1,12 @@
 const $win=$(window),
 			$el=$('.dashes').html('<path/>'),
+			ctx=$el[0].getContext('2d'),
 			PI=Math.PI, PI2=PI/2, PIx2=PI*2, PI52=PI*10000.5;
 
 
 const $path=$('path', $el);
 let touches=[], t0=performance.now(),
-	dashes=[], nx, ny, n, size;
+	dashes=[], nx, ny, n, size, thickness;
 
 $win.on('resize', e=>{
 	nx=$el.css('--w');
@@ -13,9 +14,12 @@ $win.on('resize', e=>{
 
 	if (!nx || !ny) return;
 
-	const width=$el[0].clientWidth;
-	$el.attr({width: width, height: width/nx*ny});
+	$el[0].width = $el[0].clientWidth*devicePixelRatio;
+	$el[0].height = $el[0].width/nx*ny;
 	size=$el.css('--size')||.9;
+	ctx.setTransform(devicePixelRatio, 0, 0, devicePixelRatio, 0, 0)
+	ctx.lineWidth=$el.css('--thickness');
+	ctx.strokeStyle = $el.css('color');
 
 	if (n==(n=nx*ny)) return;
 
@@ -42,10 +46,12 @@ requestAnimationFrame(function anim(){
 				x0=box.left, y0=box.top,
 				w0=box.width/nx,
 				h0=box.height/ny,
-				hw=h0*w0/2,
-				r0=w0/2*size;
+				hw=h0*w0/3,
+				r0=w0/2*size,
+				scale=hw*400;
 
-	let path='';				
+	ctx.clearRect(0,0, box.width, box.height)
+	ctx.beginPath();				
 
 	for (let j=0; j<ny; j++) {
 	 const y=y0+h0*(j+.5);
@@ -62,16 +68,18 @@ requestAnimationFrame(function anim(){
 	 	touches.forEach(touch=>{
 			const dx=touch.clientX - x,
 						dy=touch.clientY - y,
-						r2=Math.abs(dx*dx+dy*dy-hw)/hw/200,
+						dy2=dy*dy,
+						r02=dx*dx+dy2,
+						r2=Math.abs(r02-hw)/scale,
 
-					 a1=Math.atan(1/r2)*2,//f=10*w0*h0/(dx*dx+dy*dy),
+					 a1=Math.atan(1/r2),//*1.5//f=10*w0*h0/(dx*dx+dy*dy),
 					 //a1=Math.min(f, PI2)+.16,
 					 a2=-Math.atan(dx/dy),
-					 da=angleTo(a0, a2),
+					 da=angleTo(a0, a2),//),
 					 //targ=dash._angle=a1+da*(Math.cos(da))*(1-f*r2),
 					 //targ=-Math.atan(dx/dy)*(1-f*r2),
 					 targ0=dash._trg||0;
-			f+=da*(1-Math.cos(da+da))/r2*2+angleTo(a0, a1);//
+			f+=da*(1-Math.cos(da+da))/r2+angleTo(a0, a1*a1)*6;//
 	 	})
 	 	dash.v*=Math.pow(.8, dt);
 	 	dash.v+=(f-dash.v)*dt*.3;
@@ -80,10 +88,12 @@ requestAnimationFrame(function anim(){
 		//dash.style.transform=`rotate(${}rad)`;
 		dash.dx=-r0*Math.sin(a0);
 		dash.dy=r0*Math.cos(a0);
-		path+=`M${x-x0-dash.dx} ${y-y0-dash.dy} L${x-x0+dash.dx} ${y-y0+dash.dy}, `
+
+		ctx.moveTo(x-x0-dash.dx, y-y0-dash.dy);
+		ctx.lineTo(x-x0+dash.dx, y-y0+dash.dy);
 	 }
 	}
-	$path.attr('d', path+'z');
+	ctx.stroke();
 	touches.length = 0;
 })
 function angleTo(a1, a2) {//a1 to a2
